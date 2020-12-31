01585e6d85dc1cd254b7701d592b670ccef42653
Tim van Dam <timvandamcs@gmail.com>
2020-12-31 16:10:22 +0100

    made shit work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosError } from 'axios'
+
 /**
  * Iterates through an entire steam inventory asynchronously.
  *
@@ -21,9 +23,45 @@
  *     handleItem(result);
  * }
  */
-export default async function* SteamInventoryIterator(steamId: string, appId: number, contextId: number, language = 'english', count = 5000): AsyncGenerator<SteamItem | Error> {
-    // TODO: Make requests to steam
-    return
+export default async function* SteamInventoryIterator(
+	steamId: string,
+	appId: number,
+	contextId: number,
+	language = 'english',
+	count = 5000
+): AsyncGenerator<SteamItem | Error> {
+	const url = `https://steamcommunity.com/inventory/${steamId}/${appId}/${contextId}`
+	const params: RequestParams = { l: language, count }
+	let moreItems = true
+	while (moreItems) {
+		try {
+			const response = await axios.get<SteamResponse>(url, { params })
+			const { data } = response
+			const { assets, descriptions, success, error } = data
+
+			if (error) throw new Error(error)
+			if (!success) throw new Error('Unsuccessful request')
+
+			const descriptionsByClassIdInstanceId = mapByDerivedKey(
+				descriptions,
+				({ classid, instanceid }) => `${classid}_${instanceid}`
+			)
+
+			for (const asset of assets) {
+				const { classid, instanceid } = asset
+				yield Object.assign({}, asset, descriptionsByClassIdInstanceId.get(`${classid}_${instanceid}`))
+			}
+
+			params.start_assetid = data.last_assetid
+			moreItems = !!data.more_items
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				const steamError = error?.response?.data?.error
+				yield new Error(steamError ?? error.message)
+			} else if (error instanceof Error) yield error
+			else yield new Error(error.toString())
+		}
+	}
 }
 
 /**
@@ -39,7 +77,9 @@ export default async function* SteamInventoryIterator(steamId: string, appId: nu
  * // Note that this eliminates the benefits of error handling, nor asynchronous iterations (e.g. less ram usage).
  */
 export async function getInventory(...params: Parameters<typeof SteamInventoryIterator>): Promise<SteamItem[]> {
-    return (await collectAsyncIterator(SteamInventoryIterator(...params))).filter(result => !(result instanceof Error))
+	return (await collectAsyncIterator(SteamInventoryIterator(...params))).filter(
+		(result): result is SteamItem => !(result instanceof Error)
+	)
 }
 
 /**
@@ -48,7 +88,20 @@ export async function getInventory(...params: Parameters<typeof SteamInventoryIt
  * @param asyncIterable Iterable to collect
  */
 async function collectAsyncIterator<T>(asyncIterable: AsyncIterable<T>): Promise<T[]> {
-    const result: T[] = []
-    for await (const t of asyncIterable) result.push(t)
-    return result
+	const result: T[] = []
+	for await (const t of asyncIterable) result.push(t)
+	return result
+}
+
+/**
+ * Maps a collection of object by some key that is derived using a provided method
+ * @param objects Collection of objects
+ * @param keyExtractor Function that creates a key from an object
+ */
+export function mapByDerivedKey<T, K>(objects: Iterable<T>, keyExtractor: (object: T) => K): Map<K, T> {
+	const result = new Map<K, T>()
+	for (const object of objects) {
+		result.set(keyExtractor(object), object)
+	}
+	return result
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,47 +1,72 @@
+interface SteamResponse {
+	assets: Asset[]
+	descriptions: Description[]
+	last_assetid: string
+	more_items: number
+	rwgrsn: number
+	success: number
+	total_inventory_count: number
+	error: string
+}
+
+interface RequestParams {
+	l: string
+	count: number
+	start_assetid?: string
+}
+
 /**
  * Combination of Asset and Description as returned by the Steam Inventory API
  * @see https://steamcommunity.com/inventory/STEAMID/APPID/CONTEXTID/?l=LANGUAGE&count=COUNT
  */
-interface SteamItem {
-    amount: string
-    appid: number
-    assetid: string
-    classid: string
-    contextid: string
-    instanceid: string
-    actions: Action[]
-    background_color: string
-    commodity: number
-    currency: number
-    descriptions: Description[]
-    icon_url: string
-    icon_url_large: string
-    market_actions: Action[]
-    market_hash_name: string
-    market_name: string
-    market_tradable_restriction: number
-    marketable: number
-    name: string
-    name_color: string
-    tags: Tag[]
-    tradable: number
-    type: string
-}
+type SteamItem = Asset & Description
 
-interface Action {
-    link: string
-    name: string
+interface Asset {
+	amount: string
+	appid: number
+	assetid: string
+	classid: string
+	contextid: string
+	instanceid: string
 }
 
 interface Description {
-    type: string
-    value: string
-    color: string
+	actions: Action[]
+	appid: number
+	background_color: string
+	classid: string
+	commodity: number
+	currency: number
+	descriptions: Description[]
+	icon_url: string
+	icon_url_large: string
+	instanceid: string
+	market_actions: Action[]
+	market_hash_name: string
+	market_name: string
+	market_tradable_restriction: number
+	marketable: number
+	name: string
+	name_color: string
+	tags: Tag[]
+	tradable: number
+	type: string
+}
+
+interface Action {
+	link: string
+	name: string
+}
+
+interface Description {
+	type: string
+	value: string
+	color: string
 }
 
 interface Tag {
-    category: string
-    internal_name: string
-    localized_category_name: string
-    localized_tag_name: string
+	category: string
+	internal_name: string
+	localized_category_name: string
+	localized_tag_name: string
 }
