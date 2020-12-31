fbc7e6278597bac2fc8dbc9e44f5946297f2d3bb
Tim van Dam <timvandamcs@gmail.com>
2020-12-31 18:41:01 +0100

    attempt at fixing types again

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { RequestParams, SteamItem, SteamResponse } from './types'
 
+export * from './types'
+
 /**
  * Iterates through an entire steam inventory asynchronously.
  *
